edf719531a38ceff421af3c4d38158da5dd2f998
decel337 <gengshit228@gmail.com>
2020-10-20 21:47:30 +0300

    heeeeeey

---
+++ Test11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!1");
+        }
+    }
+}
