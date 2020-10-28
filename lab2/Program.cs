a2e04a8920b6202c035b08fc2eb33b5cc557c224
decel337 <gengshit228@gmail.com>
2020-10-28 16:27:30 +0200

    lab2

---
+++ lab2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lab2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+// Given number x, y. (x, y) ?c G, G - initial set
+            Console.Write("Input x: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Input y: ");
+            double y = double.Parse(Console.ReadLine());
+            if ((y >= (1 - x) && x <= 0) || (y >= (1 - x) && y <= 0))
+                Console.Write("Belong to G");
+            else
+                Console.Write("Not belong to G");
+        }
+    }
+}
